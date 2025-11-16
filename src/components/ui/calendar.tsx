6ca39d45757bee266e1f85881451d7d5748e1c4a
Grisma Bhandari <iamgrisma@gmail.com>
2025-11-16 16:59:29 +0000

    add better date picker with flexibility, the current picker is too bad f

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -24,6 +24,7 @@ function Calendar({
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium",
+        caption_dropdowns: "flex justify-center gap-1",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
@@ -53,6 +54,9 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
+      captionLayout="dropdown-buttons"
+      fromYear={1950}
+      toYear={new Date().getFullYear()}
       components={{
         IconLeft: ({ className, ...props }) => (
           <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
