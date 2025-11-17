6880ccda0e26d046ad994373976b6187a69322aa
Grisma Bhandari <iamgrisma@gmail.com>
2025-11-17 06:48:47 +0000

    add flexibility in loan repayment. allow more modification in loan schem

--- src/components/loans/add-loan-scheme.tsx
+++ src/components/loans/add-loan-scheme.tsx
@@ -348,6 +348,30 @@ export function AddLoanScheme({ triggerButton }: AddLoanSchemeProps) {
                     </FormItem>
                   )}
                 />
+                
+                <FormField
+                  control={form.control}
+                  name="is_active"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
+                      <div className="space-y-0.5">
+                        <FormLabel className="text-base">
+                          Activate Scheme
+                        </FormLabel>
+                        <FormDescription>
+                          Inactive schemes cannot be used for new loans.
+                        </FormDescription>
+                      </div>
+                      <FormControl>
+                        <Checkbox
+                          checked={field.value}
+                          onCheckedChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+
               </div>
             </ScrollArea>
 
